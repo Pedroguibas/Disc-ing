d0510c72a877d219e5ad6e5c68e7b340c8dc4926
Pedro Guimarães Bastos <pedroguibas123@gmail.com>
2025-03-29 13:22:31 -0300

    Favicon

--- paginasPrincipais/index.php
+++ paginasPrincipais/index.php
@@ -15,7 +15,7 @@ include_once("../config/db.php");
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+        <link rel="icon" type="image/x-icon" href="../assets/favicon.ico">
         <link rel="stylesheet" type="text/css" href="../css/style.css">
     </head>
 
