28bb1cb4469fc36681632bcef1ce8ad9decae6db
Pedroguibas <pedroguibas123@gmail.com>
2025-04-08 11:35:56 -0300

    Estilização do footer

--- templates/footer-template.php
+++ templates/footer-template.php
@@ -1,4 +1,51 @@
-
+<footer>
+    <div id="footerContainer" class="container">
+        <ul id="footerLinksContainer" class="d-flex justify-content-center">
+            <li class="footerSocialLink">
+                <a href="github.com/Programetes">
+                    <button class="footerSocialLinkBtn d-flex justify-content-center align-items-center">
+                        <i class="bi bi-github"></i>
+                    </button>
+                </a>
+            </li>
+            <li class="footerSocialLink">
+                <a href="github.com/Programetes">
+                    <button class="footerSocialLinkBtn d-flex justify-content-center align-items-center">
+                        <i class="bi bi-instagram"></i>
+                    </button>
+                </a>
+            </li>
+            <li class="footerSocialLink">
+                <a href="github.com/Programetes">
+                    <button class="footerSocialLinkBtn d-flex justify-content-center align-items-center">
+                        <i class="bi bi-facebook"></i>
+                    </button>
+                </a>
+            </li>
+        </ul>
+        <div class="d-flex justify-content-center">
+            <ul class="d-flex justify-content-center gap-5 mt-4 mb-4">
+                <li class="footerLink">
+                    <a href="#welcomeBanner">Home</a>
+                </li>
+                <li class="footerLink">
+                    <a href="sobre.html">Sobre</a>
+                </li>
+                <li class="footerLink">
+                    <a href="login.html">Perfil</a>
+                </li>
+                <li class="footerLink">
+                    <a href="erros.html">Erros</a>
+                </li>
+            </ul>
+        </div>
+        <div class="d-flex justify-content-center">
+            <p class="footerEmail">
+                <a href="mailto:programetes.cqtt@gmail.com">programetes.cqtt@gmail.com</a>&copy;
+            </p>
+        </div>
+    </div>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
