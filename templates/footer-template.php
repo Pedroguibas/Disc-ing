7da773698380ba00a92b499c8852a5e845aa8062
Pedroguibas <pedroguibas123@gmail.com>
2025-04-09 09:36:36 -0300

    Responsividade do footer
    Foram feitas media querries tanto em css quanto em bootstrap para tornar o footer responsivo.

--- templates/footer-template.php
+++ templates/footer-template.php
@@ -7,21 +7,21 @@ $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . "/Disc-ing_2.0/";
     <div id="footerContainer" class="container">
         <ul id="footerLinksContainer" class="d-flex justify-content-center">
             <li class="footerSocialLink">
-                <a href="github.com/Programetes">
+                <a href="https://github.com/Programetes">
                     <button class="footerSocialLinkBtn d-flex justify-content-center align-items-center">
                         <i class="bi bi-github"></i>
                     </button>
                 </a>
             </li>
             <li class="footerSocialLink">
-                <a href="github.com/Programetes">
+                <a href="https://www.instagram.com/discing_ofc?igsh=ZDZqcGkwZXppcWVv">
                     <button class="footerSocialLinkBtn d-flex justify-content-center align-items-center">
                         <i class="bi bi-instagram"></i>
                     </button>
                 </a>
             </li>
             <li class="footerSocialLink">
-                <a href="github.com/Programetes">
+                <a href="#">
                     <button class="footerSocialLinkBtn d-flex justify-content-center align-items-center">
                         <i class="bi bi-facebook"></i>
                     </button>
@@ -29,7 +29,7 @@ $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . "/Disc-ing_2.0/";
             </li>
         </ul>
         <div class="d-flex justify-content-center">
-            <ul class="d-flex justify-content-center gap-5 mt-4 mb-4">
+            <ul class="d-flex justify-content-center gap-lg-5 gap-md-5 gap-sm-4 gap-3 mt-4 mb-4">
                 <li class="footerLink">
                     <a href="<?= $BASE_URL . 'paginasPrincipais/index.php' ?>">Home</a>
                 </li>
