8a8fac2a8ceb39af7a366964eb081749f4427f60
Pedroguibas <pedroguibas123@gmail.com>
2025-04-22 11:42:52 -0300

    Continuação da página do usuário
    Agora na página do usuário é mostrado a foto de perfil, caso exista, o username e o nome do usuário

--- paginasPrincipais/perfilDoUsuario.php
+++ paginasPrincipais/perfilDoUsuario.php
@@ -1,7 +1,8 @@
 <?php
 include_once("../config/db.php");
+session_start();
 
-$title = $_SESSION['username'] . ' ' . $_SESSION['usuarioNome'] . ' ' . $_SESSION['usuarioSobrenome']; //Trocar pelo username e nome do usuário
+$title = $_SESSION['username'] . ' (' . $_SESSION['usuarioNome'] . ' ' . $_SESSION['usuarioSobrenome'] . ')'; //Trocar pelo username e nome do usuário
 include_once("../templates/header-template.php");
 
 $stmt = $conn->prepare("SELECT
@@ -15,11 +16,15 @@ $userInfo = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
     <main id="perfilUsuarioMain">
-        <div class="container">
-            <section id="perfilInfoContainer">
-                <div class="d-flex">
-                    <div id="fotoDePerfilContainer">
-                        <img src="" alt="Foto de">
+        <div class="container d-flex flex-column align-items-center">
+            <section id="perfilInfoContainer" class="col-10">
+                <div class="d-flex align-items-center gap-3">
+                    <div id="fotoDePerfilContainer" class="col-lg-3 col-md-3 col-5">
+                        <img id="fotoDePerfil" class="w-100" src="<?= $BASE_URL ?>assets/usuarios/profilePic<?= $_SESSION['usuarioID'] ?>.jpg" alt="Foto de <?= $_SESSION['username'] ?>" onerror="if (this.src != '<?= $BASE_URL ?>assets/usuarios/unknownUser.jpg') this.src = '<?= $BASE_URL ?>assets/usuarios/unknownUser.jpg';">
+                    </div>
+                    <div class="d-flex flex-column">
+                        <h1 id="username"><?= $_SESSION['username'] ?></h1>
+                        <h2 id="nomeUsuario"><?= $_SESSION['usuarioNome'] . ' ' . $_SESSION['usuarioSobrenome'] ?></h2>
                     </div>
                 </div>
             </section>
