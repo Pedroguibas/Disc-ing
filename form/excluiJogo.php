8b49e78338f54f580c171160e6e8c7a0b1e2c532
Pedroguibas <pedroguibas123@gmail.com>
2025-05-14 09:24:46 -0300

    comando sql estava comentado

--- form/excluiJogo.php
+++ form/excluiJogo.php
@@ -4,12 +4,12 @@ $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . "/Disc-ing_2.0/";
 include_once('../templates/admHeader-template.php');
 include_once('../config/db.php');
 
-// $stmt = $conn->prepare("DELETE FROM jogo WHERE jogoID = :id");
-// $stmt->execute([':id' => $_POST['id']]);
+$stmt = $conn->prepare("DELETE FROM jogo WHERE jogoID = :id");
+$stmt->execute([':id' => $_POST['id']]);
 
 
-// unlink('../assets/Jogos/banner' . $_POST['id'] . '.jpg');
-// unlink('../assets/Jogos/cover' . $_POST['id'] . '.jpg');
+unlink('../assets/Jogos/banner' . $_POST['id'] . '.jpg');
+unlink('../assets/Jogos/cover' . $_POST['id'] . '.jpg');
 ?>
 <main class="d-flex justify-content-center align-items-center" style="height: 100vh">
     <div class="d-flex flex-column p-4 gap-3 m-4" style="background: #fff; border-radius: 1em;">
